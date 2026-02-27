1b3ae45b416b34cad9e5345c1c3819be0ab803f6
Lofienjoyer <dan-kol@bk.ru>
2026-02-27 22:09:18 +0300

    feat(doctors): admin
    Add models to admin panel

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from doctors.models import Doctor
+from doctors.models import Doctor, Lpu, LpuInformation, WorkPlace
 
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Lpu)
+class LpuAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LpuInformation)
+class LpuInformationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WorkPlace)
+class WorkPlaceAdmin(admin.ModelAdmin):
     pass

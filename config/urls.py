e06b72f78aa115fb565dee9a264d3e0204f2af34
Lofienjoyer <dan-kol@bk.ru>
2026-02-28 12:54:16 +0300

    feat(config): settings
    Add supporting of static and media files

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', include('doctors.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += debug_toolbar_urls()
+    urlpatterns += (
+        debug_toolbar_urls()
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+from common_utils.decorators import sql_counter
+from doctors.models import Doctor
+
+
+@sql_counter
 def index(request):
-    return HttpResponse("Hello World</body>")
+    doctors = Doctor.objects.all()[:5]
+    context = {'doctors': doctors}
+    return render(request, 'doctors/index.html', context)
